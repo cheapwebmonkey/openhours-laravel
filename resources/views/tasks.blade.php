262a64425cfa17f55a785285a55cf2d126596523
Margeaux Spring <contact@cheapwebmonkey.com>
2016-11-17 23:48:37 -0500

    inital commit

---
+++ resources/views/tasks.blade.php
@@ -0,0 +1,208 @@
+@extends('layouts.app')
+
+@section('content')
+<div class= "container">
+	<div>
+		<svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 729.91594 348.11177">
+  <title>openhours4</title>
+  <rect x="0.5" y="0.64509" width="164.06784" height="289.29157" style="stroke: #231f20;stroke-linecap: round;stroke-miterlimit: 10"/>
+  <rect x="189" y="0.64509" width="164.06784" height="289.29157" style="stroke: #231f20;stroke-linecap: round;stroke-miterlimit: 10"/>
+  <g class="target" id ="lightSpeedIn">
+    <polygon points="377.5 42.901 541.568 0.645 541.568 289.937 382.578 256.778 377.5 42.901" style="fill: #e90046"/>
+    <polygon points="377.5 42.901 541.568 0.645 541.568 289.937 382.578 256.778 377.5 42.901" style="fill: none;stroke: #e90046;stroke-linecap: round;stroke-miterlimit: 10"/>
+  </g>
+  <rect x="564.5" y="0.64509" width="164.06784" height="289.29157" style="stroke: #231f20;stroke-linecap: round;stroke-miterlimit: 10"/>
+  <path d="M137.30273,206.582c39.04346,0,69.48682,28.03564,69.48682,64.15527,0,36.11914-30.44336,64.15527-69.48682,64.15527S67.81592,306.85645,67.81592,270.7373C67.81592,234.61768,98.25928,206.582,137.30273,206.582Zm0,84.27881a19.26367,19.26367,0,1,0-19.26367-19.26367A19.232,19.232,0,0,0,137.30273,290.86084Z" transform="translate(-39.70418 -40.32817)" style="fill: #fff"/>
+  <path d="M286.56787,208.47412h47.12744c29.92725,0,63.4668,8.6001,63.4668,44.20313,0,31.9917-26.83154,44.89111-57.61914,44.89111h-1.71973V333H286.56787V208.47412Zm52.63135,57.44678c8.77148,0,12.89941-4.64355,12.89941-10.4917,0-5.84766-4.2998-10.4917-12.89941-10.4917h-1.376v20.9834h1.376Z" transform="translate(-39.70418 -40.32817)" style="fill: #fff"/>
+  <path d="M492.56836,208.47412h89.60937v41.62354H543.82324v6.70752h30.959v29.5835h-30.959v7.05176h40.7627V333H492.56836V208.47412Z" transform="translate(-39.70418 -40.32817)" style="fill: #fff"/>
+  <path d="M637.38574,208.47412h47.52742l37.35478,53.147,0.35051-.17187v-52.9751h47.00166V333H722.09269l-37.35578-53.147-0.35051.17188V333H637.38574V208.47412Z" transform="translate(-39.70418 -40.32817)" style="fill: #fff"/>
+  <g>
+    <path d="M550.28121,336.30713H560.155v20.4502h17.00645v-20.4502h9.87474v51.84473h-9.87474V366.55029H560.155v21.60156h-9.87382V336.30713Zm31.13189,24.626H555.90434V340.48389h-1.37152V383.9751h1.37152V362.37354H581.4131V383.9751h1.37152V340.48389H581.4131v20.44922Z" transform="translate(-39.70418 -40.32817)"/>
+    <path d="M622.96426,336.019c5.76019,0,9.73769,4.17676,9.73769,10.22559v31.9707c0,6.04785-3.97749,10.22461-9.73769,10.22461H606.43844c-5.76019,0-9.73769-4.17676-9.73769-10.22461v-31.9707c0-6.04883,3.97749-10.22559,9.73769-10.22559h16.52582Zm5.48607,11.2334c0-4.46484-2.46892-7.05664-6.72053-7.05664H607.672c-4.25069,0-6.7196,2.5918-6.7196,7.05664v29.9541c0,4.46484,2.46892,7.05664,6.7196,7.05664H621.7298c4.25161,0,6.72053-2.5918,6.72053-7.05664v-29.9541Zm-6.72053-5.61621c3.42833,0,5.349,2.01563,5.349,5.61621v29.9541c0,3.60059-1.92068,5.61719-5.349,5.61719H607.672c-3.42833,0-5.34809-2.0166-5.34809-5.61719v-29.9541c0-3.60059,1.91975-5.61621,5.34809-5.61621H621.7298Zm1.0974,6.624a2.15312,2.15312,0,0,0-2.33186-2.44824H608.90643a2.15247,2.15247,0,0,0-2.33093,2.44824v27.93848a2.15247,2.15247,0,0,0,2.33093,2.44824h11.58891a2.15312,2.15312,0,0,0,2.33186-2.44824V348.26025Z" transform="translate(-39.70418 -40.32817)"/>
+    <path d="M676.85816,378.21533c0,6.04785-3.97657,10.22461-9.73676,10.22461h-15.772c-5.76019,0-9.73769-4.17676-9.73769-10.22461v-41.9082h9.87474v39.8916a2.15272,2.15272,0,0,0,2.33186,2.44824h10.83416a2.15312,2.15312,0,0,0,2.33186-2.44824v-39.8916h9.87382v41.9082Zm-5.62313-1.00879c0,3.60059-1.91975,5.61719-5.34809,5.61719H652.58386c-3.42926,0-5.349-2.0166-5.349-5.61719V340.48389h-1.37152v36.72266c0,4.46484,2.46892,7.05664,6.72053,7.05664h13.30308c4.25161,0,6.7196-2.5918,6.7196-7.05664V340.48389H671.235v36.72266Z" transform="translate(-39.70418 -40.32817)"/>
+    <path d="M712.71766,388.15186l-7.95406-19.22559h-7.955v19.22559h-9.87382V336.30713h25.16611c5.76019,0,9.73769,4.17676,9.73769,10.22559v12.16895c0,4.89648-2.606,8.56836-6.72053,9.793l8.22911,19.65723h-10.6295Zm4.32014-4.17676-7.95406-19.15332h1.7827c4.25161,0,6.72053-2.5918,6.72053-7.05664V347.54053c0-4.46484-2.46892-7.05664-6.72053-7.05664h-19.68V383.9751h1.37152V364.82178h15.08577l7.95406,19.15332h1.44Zm-0.82236-26.21c0,3.60059-1.92068,5.61621-5.349,5.61621H692.55792v-21.457h18.30852c3.42833,0,5.349,2.01563,5.349,5.61621v10.22461Zm-6.58347,1.5127a2.15312,2.15312,0,0,0,2.33186-2.44824v-8.28125a2.15312,2.15312,0,0,0-2.33186-2.44824H696.80861v13.17773H709.632Z" transform="translate(-39.70418 -40.32817)"/>
+    <path d="M754.54387,348.26025a2.15247,2.15247,0,0,0-2.33093-2.44824h-9.87474a2.15247,2.15247,0,0,0-2.33093,2.44824v4.89648c0,1.584.82236,2.08789,2.33093,2.44824l12.75484,2.87988c5.34809,1.22461,9.87474,4.6084,9.87474,10.36914v9.36133c0,6.04785-3.97749,10.22461-9.73769,10.22461H739.86927c-5.76021,0-9.73676-4.17676-9.73676-10.22461v-6.625h9.5997v4.6084a2.15312,2.15312,0,0,0,2.33186,2.44824h10.6971a2.15312,2.15312,0,0,0,2.33186-2.44824v-5.47266c0-1.584-.82328-2.08789-2.33186-2.44824l-12.75391-2.87988c-5.349-1.22461-9.87474-4.6084-9.87474-10.36914v-8.78418c0-6.04883,3.97655-10.22559,9.73676-10.22559h14.53754c5.76019,0,9.73769,4.17676,9.73769,10.22559v6.04785h-9.60063v-4.03223Zm6.17229,21.53027c0-3.52832-1.37152-5.40039-4.59426-6.26465l-16.1841-4.39258c-3.08568-.86426-4.18216-2.23242-4.18216-5.04v-6.84082c0-3.60059,1.91975-5.61621,5.34809-5.61621h12.2066c3.42833,0,5.34809,2.01563,5.34809,5.61621v0.86426h1.37152v-0.86426c0-4.46484-2.468-7.05664-6.71961-7.05664h-12.2066c-4.25161,0-6.7196,2.5918-6.7196,7.05664v6.84082c0,4.03223,1.71417,5.40039,5.55368,6.48047l16.1841,4.39258c2.468,0.64746,3.22274,2.44727,3.22274,4.82422v7.416c0,3.60059-1.92068,5.61719-5.349,5.61719h-13.029c-3.42833,0-5.34809-2.0166-5.34809-5.61719v-1.43945h-1.37152v1.43945c0,4.46484,2.468,7.05664,6.71961,7.05664h13.029c4.25161,0,6.72053-2.5918,6.72053-7.05664v-7.416Z" transform="translate(-39.70418 -40.32817)"/>
+  </g>
+</svg>
+</div>
+<div class = "flex-container">
+<div class ="col-xs-12">
+<script>
+
+document.addEventListener('DOMContentLoaded', function(){
+
+var width =600,
+height = .5 * width;
+
+
+var nodes = d3.range(400).map(function() { return {radius: Math.random() * 25 + 8}; }),
+root = nodes[0];
+
+root.radius = 0;
+root.fixed = true;
+
+var force = d3.layout.force()
+.gravity(0.05)
+.charge(function(d, i) { return i ? 0 : -7000; })
+.nodes(nodes)
+.size([width, height]);
+
+force.start();
+
+var canvas = d3.select("body").append("canvas")
+.attr("width", 400)
+.attr("height", 250);
+
+var context = canvas.node().getContext("2d");
+
+force.on("tick", function(e) {
+var q = d3.geom.quadtree(nodes),
+i,
+d,
+n = nodes.length;
+
+for (i = 1; i < n; ++i) q.visit(collide(nodes[i]));
+
+context.clearRect(0, 0, width, height);
+context.fillStyle = "000000";
+
+context.beginPath();
+for (i = 1; i < n; ++i) {
+d = nodes[i];
+context.moveTo(d.x, d.y);
+context.arc(d.x, d.y, d.radius, 0, 1, 2 * Math.PI);
+}
+context.fill();
+});
+
+canvas.on("mousemove", function() {
+var p1 = d3.mouse(this);
+root.px = p1[0];
+root.py = p1[1];
+force.resume();
+});
+
+function dragmove(d) {
+d3.select(this)
+.attr("cx", d.x = Math.max(radius, Math.min(width - radius, d3.event.x)))
+.attr("cy", d.y = Math.max(radius, Math.min(height - radius, d3.event.y)));
+}
+
+function collide(node) {
+var r = node.radius + 16,
+nx1 = node.x - r,
+nx2 = node.x + r,
+ny1 = node.y - r,
+ny2 = node.y + r;
+return function(quad, x1, y1, x2, y2) {
+if (quad.point && (quad.point !== node)) {
+var x = node.x - quad.point.x,
+  y = node.y - quad.point.y,
+  l = Math.sqrt(x * x + y * y),
+  r = node.radius + quad.point.radius;
+if (l < r) {
+l = (l - r) / l * .8;
+node.x -= x *= l;
+node.y -= y *= l;
+quad.point.x += x;
+quad.point.y += y;
+}
+}
+return x1 > nx2 || x2 < nx1 || y1 > ny2 || y2 < ny1;
+};
+}
+})();
+
+function updateScales() {
+  var width = d3.min([W.getViewportWidth(), maxWidth]) - rightPadding;
+  xScale.rangeBands([0, width], 0.04);
+}
+
+</script>
+
+</div>
+</div>
+<div class="jumbotron">
+  <h1>{{$ldate = date('M d, Y H:i:s')}}</h1>
+
+</div>
+    <div class="container">
+        <div class="col-sm-offset-2 col-sm-8">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h2>Are you here?</h2>
+                </div>
+
+                <div class="panel-body">
+                    <!-- Display Validation Errors -->
+                    @include('common.errors')
+
+                    <!-- New Member Form -->
+                    <form action="{{ url('task')}}" method="POST" class="form-horizontal">
+                        {{ csrf_field() }}
+
+                        <!-- Task Name -->
+                        <div class="form-group">
+                            <label for="task-name" class="col-sm-3 control-label">I'm at the space</label>
+
+                            <div class="col-sm-6">
+                                <input type="text" name="name" id="task-name" class="form-control" value="{{ old('task') }}">
+                            </div>
+                        </div>
+
+                        <!-- Add Task Button -->
+                        <div class="form-group">
+                            <div class="col-sm-offset-3 col-sm-6">
+                                <button type="submit" class="btn btn-success">
+                                    <i class="fa fa-btn fa-plus"></i>In the Hackerspace
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+
+            <!-- New Member Form -->
+            <form action="{{ url('task')}}" method="PUT" class="form-horizontal">
+                {{ csrf_field() }}
+            <!-- Current Members -->
+            @if (count($tasks) > 0)
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h2>People currently hanging out:</h2>
+                    </div>
+
+                    <div class="panel-body">
+                        <table class="table table-striped task-table">
+                            <thead>
+                                <th><em>And yes. The laser printer is probably broken.</em></th>
+                                <th>&nbsp;</th>
+                            </thead>
+                            <tbody>
+                                @foreach ($tasks as $task)
+                                    <tr>
+                                        <td class="table-text"><div>{{ $task->name }}</div></td>
+
+                                        <!-- Task Delete Button -->
+                                        <td>
+                                            <form action="{{ url('task/'.$task->id) }}" method="POST">
+                                                {{ csrf_field() }}
+
+                                                {{ method_field('DELETE') }}
+
+                                                <button type="submit" class="btn btn-danger">
+                                                    <i class="fa fa-btn fa-trash"></i>Bounce
+                                                </button>
+
+                                            </form>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            @endif
+        </div>
+
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,76 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| This file is where you may define all of the routes that are handled
+| by your application. Just tell Laravel the URIs it should respond
+| to using a Closure or controller method. Build something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| This route group applies the "web" middleware group to every route
+| it contains. The "web" middleware group is defined in your HTTP
+| kernel and includes session state, CSRF protection, and more.
+|
+*/
+
+use App\Task;
+use Illuminate\Http\Request;
+
+Route::group(['middleware' => ['web']], function () {
+
+
+    /**
+     * Show Members
+     */
+    Route::get('/', function () {
+        return view('tasks', [
+            'tasks' => Task::orderBy('created_at', 'asc')->get()
+        ]);
+    });
+
+    /**
+     * Add Member Status
+     */
+    Route::post('/task', function (Request $request) {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|max:255',
+        ]);
+
+        if ($validator->fails()) {
+            return redirect('/')
+                ->withInput()
+                ->withErrors($validator);
+        }
+
+        $task = new Task;
+        $task->name = $request->name;
+        $task->save();
+
+        return redirect('/');
+    });
+
+
+    /**
+     * Delete Member Status
+     */
+    Route::delete('/task/{id}', function ($id) {
+        Task::findOrFail($id)->delete();
+
+        return redirect('/');
+    });
+});
