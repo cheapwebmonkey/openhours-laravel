0449ef639627a00c83a11b1caec87a1d32f10575
Margeaux Spring <contact@cheapwebmonkey.com>
2016-11-19 12:07:45 -0500

    fixe dmigration- maybe

--- routes/web.php
+++ routes/web.php
@@ -46,23 +46,23 @@ Route::group(['middleware' => ['web']], function () {
     /**
      * Add Member Status
      */
-    Route::post('/task', function (Request $request) {
-        $validator = Validator::make($request->all(), [
-            'name' => 'required|max:255',
-        ]);
+     Route::post('/task', function (Request $request) {
+           $validator = Validator::make($request->all(), [
+               'name' => 'required|max:255',
+           ]);
 
-        if ($validator->fails()) {
-            return redirect('/')
-                ->withInput()
-                ->withErrors($validator);
-        }
+           if ($validator->fails()) {
+               return redirect('/')
+                   ->withInput()
+                   ->withErrors($validator);
+           }
 
-        $task = new Task;
-        $task->name = $request->name;
-        $task->save();
+           $task = new Task;
+           $task->name = $request->name;
+           $task->save();
 
-        return redirect('/');
-    });
+           return redirect('/');
+       });
 
 
     /**
